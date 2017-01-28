b411fb291765a179a5734b71ec67f82a624a8042
Nikita Seleckis <nikita@seleckis.lv>
2017-01-28 16:55:37 +0200

    put back activateByKey, was broked swipeable navigation

--- src/Components.jsx
+++ src/Components.jsx
@@ -12,6 +12,9 @@ export class List extends Component{
 	constructor(props){
 		super(props);
 	}
+	handleChangeIndex = (index) => {
+		this.props.activateByKey(index);
+	};
 	render() {
 		const { container: Container, component: Component, items, activeKey, svProps, ...restProps } = this.props;
 		return Container ? (
