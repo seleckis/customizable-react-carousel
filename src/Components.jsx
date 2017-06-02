56053aea42c76aac63c58a8f968269b3b24a58c7
Nikita Seleckis <nikita@seleckis.lv>
2017-06-02 13:13:16 +0300

    replace ProptTypes from React to separate package

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 
 export class List extends Component{
